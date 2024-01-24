be4b7a8df6cafd74eb54fb60cbb486258799b3c6
Vinicios Wentz <vinicios@wentz.io>
2024-01-24 15:35:54 -0300

    first commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Vinicio Wentz on 24/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
