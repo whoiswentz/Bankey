a9cc4249d4c379eaf9d2a171ba69e675b665c3ae
Vinicios Wentz <vinicios@wentz.io>
2024-01-26 20:59:09 -0300

    username field added

--- Bankey/Login/LoginView.swift
+++ Bankey/Login/LoginView.swift
@@ -9,6 +9,13 @@ import Foundation
 import UIKit
 
 class LoginView: UIView {
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Username"
+        return textField
+    }()
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 200, height: 200)
     }
@@ -18,6 +25,7 @@ class LoginView: UIView {
         
         style()
         layout()
+        delegates()
     }
     
     required init?(coder: NSCoder) {
@@ -25,13 +33,43 @@ class LoginView: UIView {
     }
 }
 
+// MARK: - LoginView init functions
 extension LoginView {
     private func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .orange
     }
     
+    private func delegates() {
+        usernameTextField.delegate = self
+    }
+    
     private func layout() {
+        addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            usernameTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: usernameTextField.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        usernameTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if usernameTextField.text != "" {
+            return true
+        }
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
