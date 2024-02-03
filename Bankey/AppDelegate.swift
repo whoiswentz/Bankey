4916d4aa36af21d91939ce20fdd8b8121f096dc3
Vinicios Wentz <vinicios@wentz.io>
2024-02-03 01:21:05 -0300

    unit tests

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -23,13 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
         
-        let vc = mainViewController
-        vc.setStatusBar()
-        
-        UINavigationBar.appearance().isTranslucent = false
-        UINavigationBar.appearance().backgroundColor = .systemTeal
-        
-        window?.rootViewController = vc
+        displayLogin()
         
         return true
     }
@@ -37,22 +31,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        if LocalState.hasOnboarded {
-            setRootViewController(mainViewController)
-            return
-        }
-        setRootViewController(onboardingContainerViewController)
+        displayNextScreen()
     }
 }
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
 
 extension AppDelegate {
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+            return
+        }
+        setRootViewController(onboardingContainerViewController)
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
+        
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = .systemTeal
+    }
+    
     private func setRootViewController(_ vc: UIViewController, animated: Bool = true) {
         guard animated, let window = self.window else {
             self.window?.rootViewController = vc
